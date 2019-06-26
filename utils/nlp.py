25c5414cfdfd9169c06ef9678a0defe5a1c7b240
David M. Howcroft <dave.howcroft@gmail.com>
2019-06-26 17:25:08 +0100

    more normalization, incl. from TGen

--- utils/nlp.py
+++ utils/nlp.py
@@ -4,6 +4,7 @@
 ######################################################################
 ######################################################################
 import re
+import regex
 
 file = open
 
@@ -52,7 +53,7 @@ def normalize(text):
     text = re.sub(r'^\s*|\s*$', '', text)
 
     # normalize phone number
-    ms = re.findall('\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})', text)
+    ms = re.findall(r'\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})', text)
     if ms:
         sidx = 0
         for m in ms:
@@ -64,23 +65,23 @@ def normalize(text):
 
     # replace st.
     text = text.replace(';', ',')
-    text = re.sub('$\/', '', text)
+    text = re.sub(r'$\/', '', text)
     text = text.replace('/', ' and ')
 
     # replace other special characters
-    text = re.sub('[\":<>@]', '', text)
-    # text = re.sub('[\":\<>@\(\)]','',text)
+    text = re.sub(r'[\":<>@]', '', text)
+    # text = re.sub(r'[\":\<>@\(\)]','',text)
     text = text.replace(' - ', '')
 
     # insert white space before and after tokens:
-    for token in ['?', '.', ',', '!']:
+    for token in ['?', '.', ',', '!', ';', ':']:
         text = insert_space(token, text)
 
     # remove all text or word initial/final apostrophes
-    text = re.sub('^\'', '', text)
-    text = re.sub('\'$', '', text)
-    text = re.sub('\'\s', ' ', text)
-    text = re.sub('\s\'', ' ', text)
+    text = re.sub(r'^\'', '', text)
+    text = re.sub(r'\'$', '', text)
+    text = re.sub(r'\'\s', ' ', text)
+    text = re.sub(r'\s\'', ' ', text)
     # replace it's, does't, you'd ... etc
     for fromx, tox in REPLACEMENTS:
         text = ' ' + text + ' '
@@ -96,11 +97,63 @@ def normalize(text):
     tokens = text.split()
     i = 1
     while i < len(tokens):
-        if re.match(u'^\d+$', tokens[i]) and re.match(u'\d+$', tokens[i - 1]):
+        if re.match(r'^\d+$', tokens[i]) and re.match(r'\d+$', tokens[i - 1]):
             tokens[i - 1] += tokens[i]
             del tokens[i]
         else:
             i += 1
-    text = ' '.join(tokens)
+    final_toks = []
+    # print(tokens)
 
-    return text
+    for token in tokens:
+        # print(re.search(r'\d[A-Za-z]', token))
+        if re.search(r'[0-9$][A-Za-z]', token):
+            # print(token)
+            token = re.sub(r'([0-9$])([A_Za-z])', r'\1 \2', token)
+            # print(token.split())
+            final_toks += token.split()
+        elif re.search(r'[A-Za-z][0-9$]', token):
+            # print(token)
+            token = re.sub(r'([A_Za-z])([0-9$])', r'\1 \2', token)
+            # print(token.split())
+            final_toks += token.split()
+        else:
+            final_toks.append(token)
+    text = ' '.join(final_toks)
+
+    return tgen_tokenize(text)
+
+
+def tgen_tokenize(text):
+    """Tokenize the given text (i.e., insert spaces around all tokens)"""
+    toks = ' ' + text + ' '  # for easier regexes
+
+    # enforce space around all punct
+    toks = regex.sub(r'(([^\p{IsAlnum}\s\.\,−\-])\2*)', r' \1 ', toks)  # all punct (except ,-.)
+    toks = regex.sub(r'([^\p{N}])([,.])([^\p{N}])', r'\1 \2 \3', toks)  # ,. & no numbers
+    toks = regex.sub(r'([^\p{N}])([,.])([\p{N}])', r'\1 \2 \3', toks)  # ,. preceding numbers
+    toks = regex.sub(r'([\p{N}])([,.])([^\p{N}])', r'\1 \2 \3', toks)  # ,. following numbers
+    toks = regex.sub(r'(–-)([^\p{N}])', r'\1 \2', toks)  # -/– & no number following
+    toks = regex.sub(r'(\p{N} *|[^ ])(-)', r'\1\2 ', toks)  # -/– & preceding number/no-space
+    toks = regex.sub(r'([-−])', r' \1', toks)  # -/– : always space before
+
+    # keep apostrophes together with words in most common contractions
+    toks = regex.sub(r'([\'’´]) (s|m|d|ll|re|ve)\s', r' \1\2 ', toks)  # I 'm, I 've etc.
+    toks = regex.sub(r'(n [\'’´]) (t\s)', r' \1\2 ', toks)  # do n't
+
+    # other contractions, as implemented in Treex
+    toks = regex.sub(r' ([Cc])annot\s', r' \1an not ', toks)
+    toks = regex.sub(r' ([Dd]) \' ye\s', r' \1\' ye ', toks)
+    toks = regex.sub(r' ([Gg])imme\s', r' \1im me ', toks)
+    toks = regex.sub(r' ([Gg])onna\s', r' \1on na ', toks)
+    toks = regex.sub(r' ([Gg])otta\s', r' \1ot ta ', toks)
+    toks = regex.sub(r' ([Ll])emme\s', r' \1em me ', toks)
+    toks = regex.sub(r' ([Mm])ore\'n\s', r' \1ore \'n ', toks)
+    toks = regex.sub(r' \' ([Tt])is\s', r' \'\1 is ', toks)
+    toks = regex.sub(r' \' ([Tt])was\s', r' \'\1 was ', toks)
+    toks = regex.sub(r' ([Ww])anna\s', r' \1an na ', toks)
+
+    # clean extra space
+    toks = regex.sub(r'\s+', ' ', toks)
+    toks = toks.strip()
+    return toks
